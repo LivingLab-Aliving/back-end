f020465a8f2b69d68e656915ab3ae449b45615f2
Yjongbeom <yukjb5154@naver.com>
2025-12-14 11:41:27 +0900

    feat: add cors

--- src/main/java/yuseong/com/guchung/config/WebConfig.java
+++ src/main/java/yuseong/com/guchung/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.ArrayList;
@@ -21,5 +22,14 @@ public class WebConfig implements WebMvcConfigurer {
                 jacksonConverter.setSupportedMediaTypes(mediaTypes);
             }
         }
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
